16fe16a2c5383c5819d321b0847d2bc80927915b
landingmalu <landing.malu@gmail.com>
2026-02-23 13:45:51 -0300

    Switch to Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   }
